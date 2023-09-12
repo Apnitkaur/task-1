c98e7fa8c6d195714a5be46780735a6ae0398050
Apnitkaur <142976005+Apnitkaur@users.noreply.github.com>
2023-09-12 22:04:10 +1200

    add consoleapp sln

---
+++ 1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _task1
+    ;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("enter your name");
+        Console.ReadLine();
+        int a, b, i, sum = 0;
+        for (i = 1; i < 100; i++)
+        {
+            a = i % 3;
+            b = i % 5;
+            if (a == 0 || b == 0)
+            {
+                Console.Write("{0}\t", i);
+                sum = sum + i;
+
+
+            }
+
+
+        } Console.WriteLine("\nThe sum of all the multiples of 3 or 5 Below 100: {0}", sum);
+        Console.Read();
+
+    }
+}
